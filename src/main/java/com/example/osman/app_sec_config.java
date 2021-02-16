efa9c2752de9833f0a20105151a31cc3f5ab5f37
Osman Özden <ozden.osman@hotmail.com>
2021-02-16 23:30:53 +0300

    up to date

--- src/main/java/com/example/osman/app_sec_config.java
+++ src/main/java/com/example/osman/app_sec_config.java
@@ -1,2 +1,33 @@
-package com.example.osman;public class app_sec_config {
+package com.example.osman;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class app_sec_config extends WebSecurityConfigurerAdapter
+{
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		// TODO Auto-generated method stub
+		//return super.userDetailsService();
+		
+		
+		List<UserDetails> users = new ArrayList<>();
+		users.add(User.withDefaultPasswordEncoder().username("osman").password("12345").roles("user").build());
+		
+		return new InMemoryUserDetailsManager(users);
+		
+		
+	}
+
 }
--- src/main/java/com/example/osman/m_api.java
+++ src/main/java/com/example/osman/m_api.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/mesaj")
+
 public class m_api {
+    @RequestMapping("/message")
     @GetMapping
     public String merhaba(){
         return "hello world";
+    }
+    @RequestMapping("/customer")
+    @GetMapping
+    public String adress(){
+        return "adressInfo: ISTANBUL/TURKEY";
     }
 }
